38af720520ae020c219c6df266c7e9af4827df1f
par2ashish <par2ashish@gmail.com>
2023-10-15 15:07:55 +0530

    webconfig.cs modified

--- ReactApp/src/App.js
+++ ReactApp/src/App.js
@@ -2,11 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [posts, setPosts] = useState([]);
+   useEffect(() => {
+      fetch('http://localhost:8080/api/webapi1')
+         .then((response) => response.json())
+         .then((data) => {
+            console.log(data);
+            setPosts(data);
+         })
+         .catch((err) => {
+            console.log(err.message);
+         });
+   }, []);
   return (
     <div className="App">
       <header className="App-header">
-      <h1>Hello World</h1>      
-        
+      <h1>Hello World</h1>
       </header>
     </div>
   );
