a97e4b23770e3f0b5abdbe383ecc774d6178c4e9
par2ashish <par2ashish@gmail.com>
2023-10-15 15:12:35 +0530

    modified app.js

--- ReactApp/src/App.js
+++ ReactApp/src/App.js
@@ -2,19 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
-  const [posts, setPosts] = useState([]);
-   useEffect(() => {
-      fetch('http://localhost:8080/api/webapi1')
-         .then((response) => response.json())
-         .then((data) => {
-            console.log(data);
-            setPosts(data);
-         })
-         .catch((err) => {
-            console.log(err.message);
-         });
-   }, []);
   return (
     <div className="App">
       <header className="App-header">
